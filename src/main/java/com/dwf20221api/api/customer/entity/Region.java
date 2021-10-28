ac94f035c83973bd5f5d0402b85b150d5d871497
Ivan <ivan.saavedra@ciencias.unam.mx>
2021-10-28 13:06:32 -0500

    feature category & fix getters/setters entities

--- src/main/java/com/dwf20221api/api/customer/entity/Region.java
+++ src/main/java/com/dwf20221api/api/customer/entity/Region.java
@@ -16,7 +16,6 @@ import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "region")
 public class Region {
@@ -37,4 +36,45 @@ public class Region {
 	@Min(value=0, message="status must be 0 or 1")
 	@Max(value=1, message="status must be 0 or 1")
 	private Integer status;
+	
+	public Region() {
+		
+	}
+
+	public Region(Integer id_region, @NotNull(message = "region is required") String region,
+			@Min(value = 0, message = "status must be 0 or 1") @Max(value = 1, message = "status must be 0 or 1") Integer status) {
+		super();
+		this.id_region = id_region;
+		this.region = region;
+		this.status = status;
+	}
+
+	public Integer getId_region() {
+		return id_region;
+	}
+
+	public void setId_region(Integer id_region) {
+		this.id_region = id_region;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Region [id_region=" + id_region + ", region=" + region + ", status=" + status + "]";
+	}
 }
